f3b4daf5f03dc26b968741f9636305f7865b00d1
wildenali <wildenwildenaliali@gmail.com>
2020-07-07 22:18:19 +0700

    cek plugin

---
+++ bwamern/src/parts/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+import BrandIcon from 'parts/IconText'
+
+export default function Header(props {
+
+    return (
+        <header>
+            div.container
+        </header>
+    )
+
+
+})
