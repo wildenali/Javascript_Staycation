b317c4df1f0baa387143efc4611a8a6c7a962d1e
wildenali <wildenwildenaliali@gmail.com>
2020-07-08 09:00:01 +0700

    komponen header

--- bwamern/src/parts/Header.js
+++ bwamern/src/parts/Header.js
@@ -3,13 +3,45 @@ import React from 'react'
 import Button from 'elements/Button'
 import BrandIcon from 'parts/IconText'
 
-export default function Header(props {
+export default function Header(props) {
+
+    const getNavLinkClass = path => {
+        return props.location.pathname === path ? " active" : ""
+    }
 
     return (
-        <header>
-            div.container
+        <header className="spacing-sm">
+            <div className="container">
+                <nav className="navbar navbar0expand-lg navbar-light">
+                    <BrandIcon />
+                </nav>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className={`nav-item${getNavLinkClass("/")}`}>
+                            <Button className="nav-link" type="link" href="/">
+                                Home
+                            </Button>
+                        </li>
+                        <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                            <Button className="nav-link" type="link" href="/browse-by">
+                                Browse By
+                            </Button>
+                        </li>
+                        <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                            <Button className="nav-link" type="link" href="/stories">
+                                Stories
+                            </Button>
+                        </li>
+                        <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                            <Button className="nav-link" type="link" href="/agents">
+                                Agents
+                            </Button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </header>
     )
 
 
-})
+}
