b54bced7d04b26855d95b2fc3a38b30768e56884
wildenali <wildenwildenaliali@gmail.com>
2020-07-08 09:30:46 +0700

    navbar header

--- bwamern/src/App.js
+++ bwamern/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 import "assets/scss/style.scss"
+import LandingPage from 'pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      Hi Duniaa
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
