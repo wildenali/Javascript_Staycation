e7230da65e866be6b2d6cbc4e3db1da2db757885
wildenali <wildenwildenaliali@gmail.com>
2020-07-08 14:38:54 +0700

    format number di bagian travelers, treasure dan cities

--- bwamern/src/parts/Hero.js
+++ bwamern/src/parts/Hero.js
@@ -7,6 +7,7 @@ import IconTraveler from 'assets/images/icons/icon-traveler.svg'
 import IconTreasure from 'assets/images/icons/icon-treasure.svg'
 
 import Button from 'elements/Button'
+import formatNumber from 'utils/formatNumber'
 
 export default function Hero(props) {
 
@@ -25,7 +26,7 @@ export default function Hero(props) {
                         Forget Busy Work,<br />
                         Start Next Vacation
                     </h1>
-                    <p className="mb-5 font-weight-light text-gray-500 w-75" style={{ lineHeight: "170%" }}>
+                    <p className="mb-4 font-weight-light text-gray-500 w-75" style={{ lineHeight: "170%" }}>
                         We provide what you need to enjoy your holiday with familiy.
                         Time to make another memorable moments.
                     </p>
@@ -36,15 +37,15 @@ export default function Hero(props) {
                     <div className="row mt-5" style={{ marginTop: 80 }}>
                         <div className="col-auto" style={{ marginRight: 35 }}>
                             <img width="36" height="36" src={IconTraveler} alt={`${props.data.travelers} Travelers`}/>
-                            <h6 className="mt-3">{props.data.travelers}{" "} <span className="text-gray-500 font-weight-light">travelers</span></h6>
+                            <h6 className="mt-3">{formatNumber(props.data.travelers)}{" "} <span className="text-gray-500 font-weight-light">travelers</span></h6>
                         </div>
                         <div className="col-auto" style={{ marginRight: 35 }}>
                             <img width="36" height="36" src={IconTreasure} alt={`${props.data.treasures} Travelers`}/>
-                            <h6 className="mt-3">{props.data.treasures}{" "} <span className="text-gray-500 font-weight-light">treasures</span></h6>
+                            <h6 className="mt-3">{formatNumber(props.data.treasures)}{" "} <span className="text-gray-500 font-weight-light">treasures</span></h6>
                         </div>
                         <div className="col-auto">
                             <img width="36" height="36" src={IconCities} alt={`${props.data.cities} Travelers`}/>
-                            <h6 className="mt-3">{props.data.cities}{" "} <span className="text-gray-500 font-weight-light">cities</span></h6>
+                            <h6 className="mt-3">{formatNumber(props.data.cities)}{" "} <span className="text-gray-500 font-weight-light">cities</span></h6>
                         </div>
                     </div>
                 </div>
