787ae30ee7bfe6ee0dc786fe493bd3837f9dafbc
wildenali <wildenwildenaliali@gmail.com>
2020-07-16 19:26:23 +0700

    tambah model untuk file Item untuk

---
+++ server-staycation/models/Item.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    country: {
+        type: String,
+        default: 'Indonesia'
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    isPopular: {
+        type: Boolean,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageId: [{
+        type: ObjectId,
+        ref: 'Image'
+    }],
+    featureId: [{
+        type: ObjectId,
+        ref: 'Feature'
+    }],
+    activityId: [{
+        type: ObjectId,
+        ref: 'Activity'
+    }]
+})
+
+module.exports = mongoose.model('Item', itemSchema);
