481ac4db3f86683bbb2bfd0625cb64ae27052ef6
wildenali <wildenwildenaliali@gmail.com>
2020-07-17 11:03:24 +0700

    tambah halaman admin dashboard

--- server-staycation/app.js
+++ server-staycation/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// router admin
+const adminRouter = require('./routes/admin');
+
 var app = express();
 
 // view engine setup
@@ -16,12 +19,14 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());  
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));  // tambahan untuk startbootstarp untuk sbadmin2
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// admin
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
