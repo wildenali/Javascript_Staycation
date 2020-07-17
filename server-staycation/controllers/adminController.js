196af4b0d04ce5dab1adb737a51bcccb422fcdb9
wildenali <wildenwildenaliali@gmail.com>
2020-07-17 14:00:43 +0700

    tambah halaman view_category

--- server-staycation/controllers/adminController.js
+++ server-staycation/controllers/adminController.js
@@ -1,5 +1,9 @@
 module.exports = {
     viewDashboard: (req, res) => {
         res.render('admin/dashboard/view_dashboard');
-    }
+    },
+    
+    viewCategory: (req, res) => {
+        res.render('admin/category/view_category');
+    },
 }
