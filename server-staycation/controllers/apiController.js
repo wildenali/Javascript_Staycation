8bf75808b4d2308475afd0a4542d6012c2e708da
wildenali <wildenwildenaliali@gmail.com>
2020-07-23 22:26:34 +0700

    tampilin testimonial pake postman untuk menampilkannya

--- server-staycation/controllers/apiController.js
+++ server-staycation/controllers/apiController.js
@@ -43,6 +43,16 @@ module.exports = {
         }
       }
 
+      const testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content: "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Angga",
+        familyOccupation: "Product Designer"
+      }
+
       res.status(200).json({
         hero: {
           treveler: treveler.length,
@@ -50,7 +60,8 @@ module.exports = {
           cities: city.length,
         },
         mostPicked,
-        category
+        category,
+        testimonial
       });
     } catch (error) {
       console.log(error);
