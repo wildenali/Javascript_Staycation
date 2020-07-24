dc9eb4b09640a6e4a8213f46400eda8019389e1e
wildenali <wildenwildenaliali@gmail.com>
2020-07-24 22:29:12 +0700

    balikin ke sebelumnya

---
+++ bwamern/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+import "assets/scss/style.scss"
+import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Checkout from 'pages/Checkout';
+import Example from 'pages/Example';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/example" component={Example} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ bwamern/src/pages/Example.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+// import InputNumber from 'elements/Form/InputNumber';
+// import InputDate from 'elements/Form/InputDate';
+// import { InputDate, InputNumber } from 'elements/Form/index'
+
+
+// export default class Example extends Component {
+//     state = {
+//         // ----------- input Number - start --------
+//         // value: "1"
+//         // ----------- input Number - end --------
+
+//         // ----------- input Date - start --------
+//         value: {
+//             startDate: new Date(),
+//             endDate: new Date(),
+//             key: "selection"
+//         }
+//         // ----------- input Date - end --------
+//     };
+
+    
+
+//     handleChange = e => {
+//         this.setState({ [e.target.name]: e.target.value });     // handle change, mau input Number atau input Date sama aja pakai ini
+//     };
+
+//     render() {
+//         return (
+//             <div className="container">
+//                 <div
+//                     className="row align-items-center justify-content-center"
+//                     style= {{ height: "100vh" }}
+//                 >
+//                     <div className="col-auto">
+//                         {/* ----------- input Number - start -------- */}
+//                         {/* <InputNumber 
+//                             max={30}
+//                             suffix=" night"
+//                             isSuffixPlural
+//                             onChange={this.handleChange}
+//                             name="value"
+//                             value={this.state.value}
+//                         /> */}
+//                         {/* ----------- input Number - end -------- */}
+
+//                         {/* ----------- input Date - start -------- */}
+//                         <InputDate
+//                             max={30}
+//                             onChange={this.handleChange}
+//                             name="value"
+//                             value={this.state.value}
+//                         />
+//                         {/* ----------- input Date - end -------- */}
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+
+
+//  Untuk nge Test Breadcrumb
+import Breadcrumb from 'elements/Breadcrumb/index';
+
+export default class Example extends Component {
+    render() {
+        const breadcrumb = [
+            { pageTitle: "Home", pageHref: "" },
+            { pageTitle: "House Details", pageHref: "" }
+        ];
+  
+        return (
+            <div className="container">
+                <div
+                    className="row align-items-center justify-content-center"
+                    style={{ height: "100vh" }}
+                >
+                    <div className="col-auto">
+                        <Breadcrumb data={breadcrumb} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
